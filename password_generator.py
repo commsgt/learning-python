979a42daee470b0ac3fe08431c5a6c3fc19bb5c5
commsgt <68705813+commsgt@users.noreply.github.com>
2020-08-12 05:26:18 -0400

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,9 @@
+
+
+
+
+import string
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
---
+++ pswgen.py
@@ -0,0 +1,54 @@
+
+
+
+
+import string
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
+
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
+print password
